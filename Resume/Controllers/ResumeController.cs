fe9a012abcf0778ab712fd20e6de8f3fbaf39e7c
artem <kebabggbab@gmail.com>
2026-07-07 03:35:50 +0700

    feat: add ResumeController

---
+++ Resume/Controllers/ResumeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Resume.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class ResumeController : Controller
+    {
+        [HttpGet]
+        [HttpGet("/")]
+        [HttpGet("/[controller]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
